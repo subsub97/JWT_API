1010348e3fea0b04cdc70b96c3d4c94dfab89293
Ho-Da-Dak <godqhr721@gmail.com>
2023-10-27 01:45:53 +0900

    test:

--- src/main/java/com/example/pr_webb/Controller/allowURIController.java
+++ src/main/java/com/example/pr_webb/Controller/allowURIController.java
@@ -1,11 +1,14 @@
 package com.example.pr_webb.Controller;
 
+import com.example.pr_webb.dto.APIAuthCodeDTO;
+import com.example.pr_webb.service.APILoginService;
 import com.example.pr_webb.util.JWTUtil;
 import com.google.gson.Gson;
 import com.nimbusds.oauth2.sdk.TokenResponse;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,44 +17,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
-@NoArgsConstructor
+
 @RestController
 @Log4j2
+@RequestMapping(value = "/auth",produces = "application/json")
 public class allowURIController {
     @Autowired
     private final JWTUtil jwtUtil = new JWTUtil();
-    @PostMapping("/auth/google/callback")
-    public ResponseEntity<String> googleCallback(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        // code를 받아왔는지 확인한다.
-        log.info("## [REQUEST] code = {}", request.getParameter("code"));
 
-        Map<String , Object> claim = Map.of("mid","testID");
-        //Access Token 유효기간 1일
-        String accessToken = jwtUtil.generateToken(claim, 1);
+    APILoginService apiLoginService;
 
-        //Refresh Token 유효기간 30일
-        String refreshToken = jwtUtil.generateToken(claim,30);
+    public allowURIController(APILoginService apiLoginService) {
+        this.apiLoginService = apiLoginService;
+    }
 
-        Gson gson = new Gson();
-
-        Map<String ,Object> keyMap = Map.of(
-                "accessToken" , accessToken,
-                "refreshToken" , refreshToken);
-
-        String jsonStr = gson.toJson(keyMap); //gson이 json으로 만들어주는듯!?
-
-
-        System.out.println("djdjdjdjdjdjdjdjdjdjdjdjd" + jsonStr);
-
-        return new ResponseEntity<>(jsonStr, HttpStatus.OK);
+    @PostMapping("/code/{registrationId}/callback")
+    public void googleLogin(@RequestBody APIAuthCodeDTO code, @PathVariable String registrationId) {
+        // code값으 DTO를 거치지 않으면 JSON형태의 문자열로 들어오게 된다.
+        apiLoginService.socialLogin(code.getCode(), registrationId);
     }
 }
-//        try{
-//            //Access Token 얻기
-//            String accessToken = getGoogleAccessToken(request,response);
-//            // 사용자 정보 얻기
-//            getGoogleUserInfo(accessToken);
-//        } catch (Exception e) {
-//            log.error(e);
-//            throw e;
-//        }
+
+
+
